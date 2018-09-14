968e3ef3a61fc160bfad49393754ac1407f6ba10
Kostiantyn Iliushchenko <kostiantyn_iliushchenko@jabil.com>
2018-09-14 14:11:32 +0300

    Changes to Demo 3

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import psycopg2
 import json
 import random
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def get_db_connection():
     dbname = 'test'
     dbuser = 'test'
-    dbhost = 'cockroach-cockroachdb-public'
-    dbport = 26257
+    dbhost = '192.168.203.158'
+    dbport = 31280
 
     connection = psycopg2.connect(database=dbname, user=dbuser, host=dbhost, port=dbport)
     return connection
@@ -27,7 +27,8 @@ def get_all_countries(entries=None):
     cursor.execute("SELECT name, capital FROM world")
     entries = cursor.fetchall()
 
-    return render_template('get_all_countries.htm', entries=entries)
+    # return json.dumps(entries)
+    return jsonify(entries)
 
 @app.route('/get_top10_most_populated')
 def get_top10_most_populated(entries=None):
@@ -36,7 +37,7 @@ def get_top10_most_populated(entries=None):
     cursor.execute("SELECT name, population FROM world ORDER BY population DESC LIMIT 10")
     entries = cursor.fetchall()
 
-    return render_template('get_top10_most_populated.htm', entries=entries)
+    return jsonify(entries)
 
 @app.route('/get_flag_of_random_country')
 def get_flag_of_random_country(entry=None):
@@ -46,7 +47,7 @@ def get_flag_of_random_country(entry=None):
     entries = cursor.fetchall()
     entry = entries[random.randint(0, len(entries) - 1)]
 
-    return render_template('get_flag_of_random_country.htm', entry=entry)
+    return jsonify(entries)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug='true')
