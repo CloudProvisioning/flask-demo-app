66cbe68ab02303b1a4f481ceb6a1a0b2e3fc03ae
Kostiantyn Iliushchenko <kostiantyn_iliushchenko@jabil.com>
2018-09-18 12:51:28 +0300

    Finalizing Demo2 app

--- src/app.py
+++ src/app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, jsonify
 import psycopg2
 import json
 import random
-import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def get_db_connection():
     dbname = 'test'
     dbuser = 'test'
-    dbhost = '192.168.203.158'
-    dbport = 31280
+    # dbhost = '192.168.203.158'
+    # dbport = 31280
+    dbhost = 'mycockroach-cockroachdb-public'
+    dbport = 26257
 
     connection = psycopg2.connect(database=dbname, user=dbuser, host=dbhost, port=dbport)
     return connection
@@ -18,36 +21,67 @@ def get_db_connection():
 @app.route('/')
 def index():
 
-    return render_template('home.htm')
+    return jsonify([
+        'Application is working!',
+        'Available APIs:',
+        '/get_all_countries',
+        '/get_top10_most_populated',
+        '/get_flag_of_random_country'
+    ])
 
 @app.route('/get_all_countries')
-def get_all_countries(entries=None):
+def get_all_countries():
     
     cursor = get_db_connection().cursor()
-    cursor.execute("SELECT name, capital FROM world")
+    cursor.execute("SELECT name, capital, continent, area, population FROM world")
     entries = cursor.fetchall()
 
-    # return json.dumps(entries)
-    return jsonify(entries)
+    result = []
+
+    for item in entries:
+        innerItem = dict(
+            country=item[0],
+            capital=item[1],
+            continent=item[2],
+            area=item[3],
+            population=item[4]
+        )
+        result.append(innerItem)
+
+    return jsonify(result)
 
 @app.route('/get_top10_most_populated')
-def get_top10_most_populated(entries=None):
+def get_top10_most_populated():
     
     cursor = get_db_connection().cursor()
     cursor.execute("SELECT name, population FROM world ORDER BY population DESC LIMIT 10")
     entries = cursor.fetchall()
 
-    return jsonify(entries)
+    result = []
+
+    for item in entries:
+        innerItem = dict(
+            country=item[0],
+            population=item[1],
+        )
+        result.append(innerItem)
+
+    return jsonify(result)
 
 @app.route('/get_flag_of_random_country')
-def get_flag_of_random_country(entry=None):
+def get_flag_of_random_country():
     
     cursor = get_db_connection().cursor()
     cursor.execute("SELECT name, flag FROM world")
     entries = cursor.fetchall()
     entry = entries[random.randint(0, len(entries) - 1)]
 
-    return jsonify(entries)
+    result = dict(
+            country=entry[0],
+            flagUrl=entry[1],
+        )
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug='true')
